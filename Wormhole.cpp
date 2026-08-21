4603ea12eb08fa1831fa4338ff7e05b1fd36314a
Sanket01-debug <sanketkansal2001@gmail.com>
2026-08-21 16:34:13 +0530

    Refactor Dijkstra's algorithm implementation

--- Wormhole.cpp
+++ Wormhole.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <queue>
+#include <climits>
 using namespace std;
+
 int main()
 {
     int t;
@@ -16,16 +19,21 @@ int main()
         int y[101];
         int cost[101];
 
+        // Source = node 0
         int sx, sy;
         cin >> sx >> sy;
+
         x[0] = sx;
         y[0] = sy;
 
+        // Destination = node 1
         int dx, dy;
         cin >> dx >> dy;
+
         x[1] = dx;
         y[1] = dy;
 
+        // Store wormholes
         for (int i = 0; i < n; i++)
         {
             int x1, y1, x2, y2, c;
@@ -40,7 +48,9 @@ int main()
             cost[i] = c;
         }
 
+        // Graph
         int graph[101][101];
+
         for (int i = 0; i < total; i++)
         {
             for (int j = 0; j < total; j++)
@@ -49,6 +59,7 @@ int main()
             }
         }
 
+        // Wormhole edges
         for (int i = 0; i < n; i++)
         {
             int start = 2 * i + 2;
@@ -58,56 +69,61 @@ int main()
             graph[end][start] = cost[i];
         }
 
+        // Normal distance between every pair of points
         for (int i = 0; i < total; i++)
         {
             for (int j = 0; j < total; j++)
             {
                 if (graph[i][j] == -1)
                 {
-                    graph[i][j] = abs(x[i] - x[j]) + abs(y[i] - y[j]);
+                    graph[i][j] =
+                        abs(x[i] - x[j]) + abs(y[i] - y[j]);
                 }
             }
         }
 
+        // ---------------- Dijkstra ----------------
+
         int dist[101];
-        bool visited[101];
 
         for (int i = 0; i < total; i++)
         {
-            dist[i] = 100000000;
-            visited[i] = false;
+            dist[i] = INT_MAX;
         }
 
+        // {distance, node}
+        priority_queue<
+            pair<int, int>,
+            vector<pair<int, int>>,
+            greater<pair<int, int>>
+        > pq;
+
+        // Source = 0
         dist[0] = 0;
+        pq.push({0, 0});
 
-        for (int count = 0; count < total; count++)
+        while (!pq.empty())
         {
+            int d = pq.top().first;
+            int node = pq.top().second;
 
-            int u = -1;
-            int minDist = 100000000;
+            pq.pop();
 
-            for (int i = 0; i < total; i++)
-            {
-                if (!visited[i] && dist[i] < minDist)
-                {
-                    minDist = dist[i];
-                    u = i;
-                }
-            }
-
-            visited[u] = true;
-
+            // Check all neighbours
             for (int v = 0; v < total; v++)
             {
-                if (!visited[v] &&
-                    dist[u] + graph[u][v] < dist[v])
-                {
+                int wt = graph[node][v];
 
-                    dist[v] = dist[u] + graph[u][v];
+                if (d + wt < dist[v])
+                {
+                    dist[v] = d + wt;
+
+                    pq.push({dist[v], v});
                 }
             }
         }
 
+        // Destination = 1
         cout << dist[1] << endl;
     }
 
